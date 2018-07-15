2c68c71fac3971185c7d0f92f4f0a5d95a562907
gafilipoie <marius.afilipoie@gmail.com>
2018-07-15 21:32:55 +0300

    reverted last commit

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { defineCustomElements as uiLib } from '@ui-guys/stencil-bs-ui-lib/dist/uiLib';
-import { defineCustomElements as formGen } from '@ui-guys/form-generator/dist/formGenerator';
-
 if (environment.production) {
     enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.log(err));
-uiLib(window);
-formGen(window);
